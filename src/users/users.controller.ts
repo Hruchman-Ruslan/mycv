634df5e8c4ba99d800a84328ba9ff6cd7f513561
Ruslan <Giovingo92@gmail.com>
2024-11-19 17:04:10 +0200

    feat: handling user sign in

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -24,9 +24,14 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
-  @Post('signup')
+  @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.authService.signup(body.email, body.password);
+  }
+
+  @Post('/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
   }
 
   @Get('/:id')
